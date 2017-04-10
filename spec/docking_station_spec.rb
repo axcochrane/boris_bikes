5bd4470a059d662bd00f5dcf5106d45aabde0f4e
Alex Chalk <alexchalk17@gmail.com>
2017-04-10 13:51:31 -0400

    Adds test that checks whether docking_station.release_bike returns a bike instance.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,13 @@
 require "docking_station"
 
 describe DockingStation do
+  alias_method :docking_station, :subject 
   describe "when created" do
     it { is_expected.to respond_to(:release_bike) }
+  end
+  describe "when asked for a bike" do
+    it "returns a bike when asked to release_bike" do
+      expect(docking_station.release_bike).to eq Bike.new
+    end
   end
 end  
