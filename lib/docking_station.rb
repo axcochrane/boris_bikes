a2848ec139113936172c69f1a4743f0e5ebd404f
Alex Chalk <alexchalk17@gmail.com>
2017-04-10 14:23:31 -0400

    Checks whether bike released by docking station is working; passes test.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,9 @@
+require "bike"
+
 class DockingStation
   def release_bike
+    bike = Bike.new
+    bike.working?
+    bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,7 +7,11 @@ describe DockingStation do
   end
   describe "when asked for a bike" do
     it "returns a bike when asked to release_bike" do
-      expect(docking_station.release_bike).to eq Bike.new
+      expect(docking_station.release_bike).to be_an_instance_of(Bike)
+    end
+
+    it "expects the bike it returns to be working" do
+      expect(docking_station.release_bike).to be_working
     end
   end
 end  
